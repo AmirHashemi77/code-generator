f1d67f6dca5af7678e097c7fdf2c59a7b694adcc
AmirHashemi77 <amirhashemi539@gmail.com>
2025-07-18 22:21:01 +0330

    add form component

---
+++ src/components/UI/Form/Form.tsx
@@ -0,0 +1,11 @@
+import { type FC, type ReactNode } from "react";
+import style from "./form.module.css";
+
+interface PropsType {
+  children: ReactNode;
+}
+
+const Form: FC<PropsType> = () => {
+  return <div></div>;
+};
+export default Form;
